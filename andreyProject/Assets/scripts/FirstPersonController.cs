6c99d447377f18f690bd061c04620c43fcc31897
gratom <gratomov@gmail.com>
2020-09-27 17:56:25 +0300

    player moving + scene

---
+++ andreyProject/Assets/scripts/FirstPersonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public float speed;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position - transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position - transform.right * Time.deltaTime * speed;
+        }
+    }
+}
