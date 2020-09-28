2700ce417779ecdc03670c5b5e739b86d1a0e526
gratom <gratomov@gmail.com>
2020-09-28 20:41:59 +0300

    Mouse control camera rotate

--- andreyProject/Assets/scripts/FirstPersonController.cs
+++ andreyProject/Assets/scripts/FirstPersonController.cs
@@ -6,12 +6,16 @@ public class FirstPersonController : MonoBehaviour
 {
     public float speed;
 
+    public GameObject personCamera;
+
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
+        transform.Rotate(new Vector2(0, Input.GetAxis("Mouse X")));
         if (Input.GetKey(KeyCode.W))
         {
             transform.position = transform.position + transform.forward * Time.deltaTime * speed;
@@ -28,5 +32,6 @@ public class FirstPersonController : MonoBehaviour
         {
             transform.position = transform.position - transform.right * Time.deltaTime * speed;
         }
+        personCamera.transform.Rotate(new Vector2(Input.GetAxis("Mouse Y") * -1, 0));
     }
 }
