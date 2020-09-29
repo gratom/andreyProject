2518b3a950cacbdf2a8f34918162a14969be7a95
gratom <gratomov@gmail.com>
2020-09-29 18:34:52 +0300

    new scene, space base, skybox, asteroid, ship control VIP

---
+++ andreyProject/Assets/MY/scripts/space game/ShipController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public GameObject cameraGameObject;
+    public float speed;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        ShipControl();
+        CameraFollower();
+    }
+
+    public void CameraFollower()
+    {
+        cameraGameObject.transform.position = gameObject.transform.position + new Vector3(0, 1.52f, -2.313f);
+    }
+
+    public void ShipControl()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.AddForce(transform.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.AddForce(-transform.forward * Time.deltaTime * speed);
+        }
+    }
+}
