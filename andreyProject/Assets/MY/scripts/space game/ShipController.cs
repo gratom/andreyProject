8dd1f9296809a62a52076c722b740ac772177325
Gratom\Maks <info@whimsy.software>
2020-10-08 20:13:43 +0300

    camera rotate, ship rotate WIP

--- andreyProject/Assets/MY/scripts/space game/ShipController.cs
+++ andreyProject/Assets/MY/scripts/space game/ShipController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
@@ -8,19 +6,38 @@ public class ShipController : MonoBehaviour
     public GameObject cameraGameObject;
     public float speed;
 
+    public Vector3 angle;
+    public Vector3 angle2;
+
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        angle = cameraGameObject.transform.rotation.eulerAngles - transform.rotation.eulerAngles;
     }
 
     private void Update()
     {
         ShipControl();
         CameraFollower();
+        CameraRotate();
+
+        angle2 = cameraGameObject.transform.rotation.eulerAngles - angle;
     }
 
     public void CameraFollower()
     {
         cameraGameObject.transform.position = gameObject.transform.position + new Vector3(0, 1.52f, -2.313f);
+    }
+
+    public void CameraRotate()
+    {
+        //izmenenie povorota cameri na raznitcu mezhdu polozheniyami cursora vo vremya smeny kadrov.
+        cameraGameObject.transform.Rotate(new Vector2(Input.GetAxis("Mouse Y") * -1, Input.GetAxis("Mouse X")));
+        //vector3 = nazvanie "rotation", zadal ravenstvo k povorotu kameri.
+        Vector3 rotation = cameraGameObject.transform.localRotation.eulerAngles;
+        rotation.z = 0;
+        //prisvoenie localnogo povorota kamery k soderjimomu "()"
+        cameraGameObject.transform.localRotation = Quaternion.Euler(rotation);
     }
 
     public void ShipControl()
