49051411f5bc802c9a513864a4c2290e14cedaaa
Gratom\Maks <info@whimsy.software>
2020-10-09 20:00:56 +0300

    Asteroids spawn

---
+++ andreyProject/Assets/MY/scripts/space game/AsteroidManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour
+{
+    public Transform point1;
+    public Transform point2;
+    public GameObject AsteroidPrefab;
+    public int AsteroidCount;
+
+    private void Start()
+    {
+        for (int i = 0; i < AsteroidCount; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(point1.position.x, point2.position.x), Random.Range(point1.position.y, point2.position.y), Random.Range(point1.position.z, point2.position.z));
+            Instantiate(AsteroidPrefab, randomPos, Quaternion.Euler(randomPos));
+        }
+    }
+
+    private void Update()
+    {
+    }
+}
