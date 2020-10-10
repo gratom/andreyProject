5509ee8b0b73ca0cdcbf927909537f394ce32037
Gratom\Maks <info@whimsy.software>
2020-10-10 15:45:02 +0300

    Asteroids scale and physic

--- andreyProject/Assets/MY/scripts/space game/AsteroidManager.cs
+++ andreyProject/Assets/MY/scripts/space game/AsteroidManager.cs
@@ -12,7 +12,9 @@ public class AsteroidManager : MonoBehaviour
         for (int i = 0; i < AsteroidCount; i++)
         {
             Vector3 randomPos = new Vector3(Random.Range(point1.position.x, point2.position.x), Random.Range(point1.position.y, point2.position.y), Random.Range(point1.position.z, point2.position.z));
-            Instantiate(AsteroidPrefab, randomPos, Quaternion.Euler(randomPos));
+            Vector3 randomScale = new Vector3(Random.Range(point1.localScale.x, point2.localScale.x), Random.Range(point1.localScale.y, point2.localScale.y), Random.Range(point1.localScale.z, point2.localScale.z));
+            GameObject G = Instantiate(AsteroidPrefab, randomPos, Quaternion.Euler(randomPos));
+            G.transform.localScale = randomScale;
         }
     }
 
